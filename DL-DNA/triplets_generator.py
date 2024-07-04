43481fd5ed801136520e0fe9d7caf9a79b0b0570
ch9968 <ichanhui578@gmail.com>
2024-07-04 23:57:37 +0900

    데이터 개수 입력 추가

--- DL-DNA/triplets_generator.py
+++ DL-DNA/triplets_generator.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import random
 
 def generate_new_triplet_file(images_dir, original_triplet_file, new_triplet_file):
     with open(original_triplet_file, 'r') as file:
@@ -18,13 +19,16 @@ def generate_new_triplet_file(images_dir, original_triplet_file, new_triplet_fil
         for triplet in new_triplets:
             file.write(' '.join(os.path.splitext(image)[0] for image in triplet) + '\n')
 
-def filter_triplet_file(new_triplet_file):
+def filter_triplet_file(new_triplet_file, num_triplets):
     with open(new_triplet_file, 'r') as file:
         triplets = [line.strip().split() for line in file.readlines()]
     valid_triplets = [triplet for triplet in triplets if triplet[2] != triplet[0] and triplet[2] != triplet[1]]
 
+    num_triplets = min(num_triplets, len(valid_triplets))
+    selected_triplets = random.sample(valid_triplets, num_triplets)
+
     with open(new_triplet_file, 'w') as file:
-        for valid_triplet in valid_triplets:
+        for valid_triplet in selected_triplets:
             file.write(' '.join(valid_triplet) + '\n')
 
 if __name__ == "__main__":
@@ -35,5 +39,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    num_triplets = int(input("학습시키고 싶은 데이터의 개수를 입력하세요: "))
+
     generate_new_triplet_file(args.images_dir, args.original_triplet_file, args.new_triplet_file)
-    filter_triplet_file(args.new_triplet_file)
+    filter_triplet_file(args.new_triplet_file, num_triplets)
